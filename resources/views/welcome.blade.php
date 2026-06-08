66058139dcea988de3545bc30a0951bd1d84e8fa
haseebk06 <haseebawan.1560@gmail.com>
2026-06-08 21:17:26 +0500

    blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>M.Riaz & Co</title>
+    <title>Pos Backend</title>
 </head>
 
 <body>
